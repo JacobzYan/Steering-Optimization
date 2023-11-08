d6701510d4b0690da08dd8cb77baa383f1afefe5
JacobzYan <70812157+JacobzYan@users.noreply.github.com>
2023-11-08 17:08:30 -0600

    Set Up Initial Interface
    Created a usable interface for optimization on steering implementation

--- SteeringSimulation.py
+++ SteeringSimulation.py
@@ -1,2 +1,39 @@
 # Simulation Function
 # Takes in geometry object and outputs matched numerical vectors of objects
+
+# from SteeringGeometry import SteeringGeometry as str
+from SteeringGeometry import SteeringGeometry as str
+
+# Returns an error value for given geometry
+def sim(geom: str):
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+    ## Fake Error value, should cause all values to converge to 50
+    # print(f'the works: {geom.vars()}')
+    error = 0
+    for i in geom.vars():
+        try:
+            error += (float(geom.vars()[i]) - 50.0)**2
+        except:
+            pass
+
+    return error
+
+
+
+
+
