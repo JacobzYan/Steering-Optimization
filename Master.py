b1c35cfac4ba95e83d3ac6cc1f023df5788eb596
thouy1 <142754769+thouy1@users.noreply.github.com>
2023-11-11 17:33:22 -0600

    Master.py

--- Master.py
+++ Master.py
@@ -10,6 +10,10 @@ Copyright: Fri Nov 10 20:03:07 2023, Tristan Houy, All rights reserved.
 from numba import jit, prange
 import numpy as np
 
+# Simulation parameters
+num_steps = 101 # Define the number of steps
+num_fit_points = 100 # Define granularity
+
 # Constants
 wb = 1500 # Wheelbase [mm]
 x_travel = 30 # Steering rack travel [mm]
@@ -20,15 +24,20 @@ l_rack = 300 # Steering rack length [mm]
 wt = (w_track - l_rack)/2 # Equivalent steering thickness [mm]
 
 # Variables
-#rack_spacing = 200 # Front/back distance between steering rack axis and control arm bearing mounting
-#l_tierod = 250 # Tierod length [mm]
-#l_str_arm = 100 # distance from control arm mounts to steering arm mount [mm]
-#phi = 11*np.pi/180 # Steering Arm offset angle [deg]
-@jit(nopython=True,parallel=True) 
+rack_spacing_lower =  100 # Front/back distance between steering rack axis and control arm bearing mounting [mm]
+rack_spacing_upper =  300
+
+l_tierod_lower = 150 # Tierod length [mm]
+l_tierod_upper = 350
+
+l_str_arm_lower = 1 # Distance from control arm mounts to steering arm mount [mm]
+l_str_arm_upper = 201
+
+@jit(nopython=True,parallel=False) 
 def cartesian_product_on_the_fly_mm(num_of_step,num_fit_points):
-    rack_spacing=np.linspace(800, 801, num_of_step)
-    l_tierod=np.linspace(600, 601, num_of_step)
-    l_str_arm=np.linspace(400, 401, num_of_step)
+    rack_spacing=np.linspace(rack_spacing_lower, rack_spacing_upper, num_of_step)
+    l_tierod=np.linspace(l_tierod_lower, l_tierod_upper, num_of_step)
+    l_str_arm=np.linspace(l_str_arm_lower, l_str_arm_upper, num_of_step)
     
     results = []
     
@@ -47,8 +56,8 @@ def cartesian_product_on_the_fly_mm(num_of_step,num_fit_points):
 # Takes in geometry object and outputs matched numerical vectors of objects
 
 # Returns an error value for given geometry
-# Inputs: geom -> Steering Geometry object, num_fit_points -> The length of the output vectors
-# Outputs: a vector of inner wheel angles, a vector of outer wheel angles, a vector of ideal outer wheel angles
+# Inputs: Steering geometry values and num_fit_points (the length of the output vectors)
+# Outputs: A vector of inner wheel angles, a vector of outer wheel angles, a vector of ideal outer wheel angles
 @jit(nopython=True, parallel=True)
 def sim(rack_spacing, wt, l_tierod, l_str_arm, wb, x_travel, num_fit_points):
     
@@ -70,7 +79,15 @@ def sim(rack_spacing, wt, l_tierod, l_str_arm, wb, x_travel, num_fit_points):
     # Determine ideal theta_o
     theta_o_ideal = theta_o_ideal_eq(wb, wt, theta_i)
 
-    return theta_i, theta_o, theta_o_ideal
+    # Preallocate results array
+    results = np.empty((3, num_fit_points), dtype=np.float64)
+
+    # Assign the results directly
+    results[0, :] = theta_i
+    results[1, :] = theta_o
+    results[2, :] = theta_o_ideal
+
+    return results
 
 # Helper Functions
 # Jit has not found a way to parallelize this
@@ -108,6 +125,53 @@ def RMSE(y_actual_np_vector,y_predicted_np_vector):
     return rmse
 
 # Running the simulation
-num_steps = 5 # Define the number of steps
-num_fit_points = 100
 simulation_results = cartesian_product_on_the_fly_mm(num_steps,num_fit_points)
+
+# Running the error calculation
+# Initialize a list to store RMSE results
+rmse_results = []
+
+# Iterate through each result in simulation_results
+for result in simulation_results:
+    # Extract the second row (actual values)
+    y_actual_np_vector = result[1, :]
+    
+    # Extract the third row (predicted values)
+    y_predicted_np_vector = result[2, :]
+
+    # Calculate RMSE and append to the rmse_results list
+    rmse = RMSE(y_actual_np_vector, y_predicted_np_vector)
+    rmse_results.append(rmse)
+
+# rmse_results now contains the RMSE for each simulation result
+
+# Finding the minimum value in the RMSE list
+min_rmse = min(rmse_results)
+min_rmse_index = rmse_results.index(min_rmse)
+
+print("Minimum RMSE:", min_rmse, "at index", min_rmse_index)
+
+
+# Optimal Geometry
+# The total number of combinations
+total_combinations = num_steps ** 3
+
+# Calculate the index in each dimension
+index_rack_spacing = (min_rmse_index // (num_steps ** 2)) % num_steps
+index_l_tierod = (min_rmse_index // num_steps) % num_steps
+index_l_str_arm = min_rmse_index % num_steps
+
+# Generate the arrays for variables
+rack_spacing_array = np.linspace(rack_spacing_lower, rack_spacing_upper, num_steps)
+l_tierod_array = np.linspace(l_tierod_lower, l_tierod_upper, num_steps)
+l_str_arm_array = np.linspace(l_str_arm_lower, l_str_arm_upper, num_steps)
+
+# Retrieve the optimal values
+optimal_rack_spacing = rack_spacing_array[index_rack_spacing]
+optimal_l_tierod = l_tierod_array[index_l_tierod]
+optimal_l_str_arm = l_str_arm_array[index_l_str_arm]
+
+print("Optimal Geometry:")
+print("Rack Spacing:", optimal_rack_spacing)
+print("Tierod Length:", optimal_l_tierod)
+print("Steering Arm Length:", optimal_l_str_arm)
